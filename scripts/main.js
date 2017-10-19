18a43de2c6d018c03157ae1d2eb200da935de62a
tstomek <tstomek@live.com>
2017-10-19 19:01:22 +0200

    ok

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,26 @@
 $(document).ready(function(){
+
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    // $('#modal1').modal('open');
+    $('.modal').modal();
+
+//  $('#modal1').modal('open');
+    $('#menu .btn').click(function(){
+        $('#myModal').modal('open');
+    })
     //ukrycie zbędnych elementów
-    $('.row').hide();
+    $('#menu .row').hide();
     $('.badge').hide();
     $('#menu-1').css("display", "");
     $('.alert').hide();
+    Materialize.updateTextFields();
+
 
     //funkcja wyświetlająca modal
     $('.btn-primary').click(function(){
         var pizzaId = this.id.split("-")[1]; //uzyskanie id pizzy z atrybutu id w buttonie
-        $('input[type="hidden"]').val(pizzaId); //id przydatne do ajaxa później
-        $('#myModal').modal('show'); //wyświetl modal
+        $('input[type="hidden"').val(pizzaId); //id przydatne do ajaxa później
+        //      $('#myModal').modal('show'); //wyświetl modal
         var contentCardTitle = $(this).parent().find('.card-title').text();
         $('#exampleModalLabel').text(contentCardTitle);
     });
@@ -27,17 +38,16 @@ $(document).ready(function(){
             var newOrder = document.createElement('li');
             newOrder.className = 'list-group-item row';
             var pizzaName = document.createElement('div');
-            pizzaName.className = 'col';
+            pizzaName.className = 'col-md-2';
             newOrder.textContent = $('#exampleModalLabel').text();
             newOrder.appendChild(pizzaName);
             $('.ingredient:checked').each(function(i, element){
                 ingredients += $(element).parent().parent().find('.form-control').val();
                 ingredients += ', ';
             });
-            console.log(ingredients);
             if (ingredients != '') {
                 var ingredientsDiv = document.createElement('div');
-                ingredientsDiv.className = 'col';
+                ingredientsDiv.className = 'col-md-2';
                 ingredientsDiv.textContent = ingredients;
                 newOrder.appendChild(ingredientsDiv);
             }
@@ -57,6 +67,34 @@ $(document).ready(function(){
         $('#menu-' + itemId).show(); //wyświetlenie nowego stylu
 
     })
+
+    $('input.autocomplete').autocomplete({
+        data: {
+            "Warszawa": null,
+            "Wrocław": null,
+            "Legnica": null,
+            "Leśnica": null,
+        },
+        limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+        onAutocomplete: function(val) {
+            // Callback function when value is autcompleted.
+        },
+        minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+    });
+    $('.chips-autocomplete').material_chip({
+        autocompleteOptions: {
+            data: {
+                'Apple': null,
+                'Microsoft': null,
+                'Google': null
+            },
+            limit: Infinity,
+            minLength: 1
+        }
+    });
+    $(document).ready(function(){
+        $('.parallax').parallax();
+    });
 });
 
 
